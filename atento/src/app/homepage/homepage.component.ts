154ff91df6b7becc3a0cfd22197e407d9baf43a0
joannaatento <06802182@dwc-legazpi.edu>
2023-02-11 12:53:27 +0800

    new

--- atento/src/app/homepage/homepage.component.ts
+++ atento/src/app/homepage/homepage.component.ts
@@ -1,18 +1,52 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { Auth } from '@angular/fire/auth';
-import { Database} from '@angular/fire/database';
+import { Database,ref, update, remove} from '@angular/fire/database';
 import { Router } from '@angular/router';
+import 'firebase/compat/database'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+
+  users!: Observable<any[]>;
+  constructor(public auth: Auth,public database: Database, private db: AngularFireDatabase,private router:Router) {
+    this.users = db.list('/users').valueChanges();
+   }
+   
+  ngOnInit(): void {
+
+
+  }
+
+
+  del(value: any){
+    remove(ref(this.database, 'users/' + value));
+    alert('Deleted Successfully')
+  }
+
+   update(value:any){
  
-    constructor(public auth: Auth,public database:Database,private router:Router) { }
-  
-    ngOnInit(): void {
-    }
+    update(ref(this.database, 'users/' + value.username), {
+       password: value.password
+     }); 
+    alert('User updated!');
+      
+  }
+
+
+  username = '';
+  fillForm(username: any) {
+    this.username = username;
+  }
+
+
+
+  password: any;
+  itemId: any;
 
 }
