9bb21bfdbc766c47b78525f9af38a68520c9f81a
joannaatento <06802182@dwc-legazpi.edu>
2023-02-20 21:21:23 +0800

    CRUD

--- atento/src/app/login/login.component.ts
+++ atento/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { Auth, signInWithEmailAndPassword} from '@angular/fire/auth';
-import { Database, ref, update } from '@angular/fire/database';
+import { Database, ref, update,onValue, remove} from '@angular/fire/database';
 import { Router } from '@angular/router';
 
 @Component({
@@ -17,30 +17,51 @@ export class LoginComponent implements OnInit{
 
   loginUser(value: any) {
 
-    signInWithEmailAndPassword(this.auth, value.email, value.password)
-      .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
+    //login 
+         signInWithEmailAndPassword(this.auth, value.email, value.password)
+        .then((userCredential) => {
+      
+          const user = userCredential.user;
 
-        alert('user login');
-        const date = new Date();
-        update(ref(this.database, 'users/' + user.uid), {
+          alert('user login');
+          const date = new Date();
+          update(ref(this.database, 'users/' + user.uid), {
 
           last_login: date
         });
 
-        this.router.navigate(['/signup'])
+       this.router.navigate(['/signup'])
       
     },err=>{
-      alert(err.message)
+     alert(err.message)
   
-    })
+   })
       
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
+     .catch((error) => {
+       const errorCode = error.code;
+       const errorMessage = error.message;
+     });
+    //read user
 
-      });
+     // const starCountRef = ref(this.database, 'users/' + value.email);
+      //onValue(starCountRef, (snapshot) => {
+      //const data = snapshot.val();
+   
+      //alert(data.email);
+   
+      //update user
+
+//update(ref(this.database, 'users/' + value.email), {
+// password: value.password
+//});
+//alert('password updated!');
+
+//remove user
+
+//remove(ref(this.database, 'users/' + value.email));
+//alert('Successfully Removed');
+
+
   }
 
   
