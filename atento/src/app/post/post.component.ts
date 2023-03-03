99a0145fd8026f527a17ba42c0473fc0bfef9091
joannaatento <06802182@dwc-legazpi.edu>
2023-03-03 18:32:18 +0800

    editdelete

---
+++ atento/src/app/post/post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent {
+
+}
